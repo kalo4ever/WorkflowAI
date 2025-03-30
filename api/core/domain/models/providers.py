045e64492a0f2ca1a30d789c703feff7aa1cbdc5
Guillaume Aquilina <guillaume@workflowai.com>
2025-03-30 07:40:55 -0400

    feat: make anthropic the default provider for anthropic models

--- api/core/domain/models/providers.py
+++ api/core/domain/models/providers.py
@@ -6,13 +6,16 @@ from enum import Enum
 # changes it will get to be selected
 class Provider(str, Enum):
     FIREWORKS = "fireworks"
+    # Anthropic is the default provider for Anthropic models
+    # Bedrock is ok but it throttles instead of returning 429s
+    # Which is very inconvenient
+    ANTHROPIC = "anthropic"
     AMAZON_BEDROCK = "amazon_bedrock"
     # OpenAI is the default provider for OpenAI models
     # We tried Azure for a while but it is no way reliable
     OPEN_AI = "openai"
     AZURE_OPEN_AI = "azure_openai"
     GOOGLE = "google"
-    ANTHROPIC = "anthropic"
     GROQ = "groq"
     MISTRAL_AI = "mistral_ai"
     GOOGLE_GEMINI = "google_gemini"
