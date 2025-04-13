bebaeaed58a3092f32b78ddcbcf5ea7d1f040d39
Guillaume Aquilina <guillaume@workflowai.com>
2025-04-13 16:51:53 +0200

    feat: add compute preview with files

--- api/core/utils/models/previews.py
+++ api/core/utils/models/previews.py
@@ -1,6 +1,7 @@
+import logging
 from typing import Any
 
-from pydantic import BaseModel, RootModel
+from pydantic import BaseModel
 
 
 class _MaxLenReached(Exception):
@@ -21,10 +22,12 @@ class _Agg:
             return "true" if value else "false"
         return str(value).replace("\n", " ")
 
-    def append(self, val: Any):
+    def append(self, val: Any, cut_on_max_len: bool = True):
         s = self._stringify(val)
         if self.remaining < len(s):
-            self.agg.append(s[: self.remaining])
+            if cut_on_max_len:
+                s = s[: self.remaining]
+            self.agg.append(s)
             raise _MaxLenReached()
         self.agg.append(s)
         self.remaining -= len(s)
@@ -34,56 +37,83 @@ class _Agg:
     def __str__(self) -> str:
         return "".join(self.agg)
 
+    def _append_any(self, value: Any):
+        if isinstance(value, dict):
+            self._append_dict(value, brackets=True)  # pyright: ignore [reportUnknownArgumentType]
+        elif isinstance(value, list):
+            self.append("[")
+            self._append_list(value)  # pyright: ignore [reportUnknownArgumentType]
+            self.append("]")
+        elif isinstance(value, str):
+            self.append(f'"{value}"')
+        else:
+            self.append(value)
 
-def _any_preview(value: Any, agg: _Agg):
-    if isinstance(value, dict):
-        agg.append("{")
-        _dict_preview(value, agg)  # type: ignore
-        agg.append("}")
-    elif isinstance(value, list):
-        agg.append("[")
-        _list_preview(value, agg)  # type: ignore
-        agg.append("]")
-    elif isinstance(value, str):
-        agg.append(f'"{value}"')
-    else:
-        agg.append(value)
+    def _append_file(self, content_type: str, url: str | None, storage_url: str | None):
+        if url and not url.startswith("https://"):
+            url = storage_url
+        if not url:
+            url = ""
 
+        match content_type.split("/")[0]:
+            case "image":
+                prefix = "img"
+            case "audio":
+                prefix = "audio"
+            case _:
+                prefix = "file"
+        self.append(f"[[{prefix}:{url}]]", cut_on_max_len=False)
 
-def _dict_preview(d: dict[str, Any], agg: _Agg):
-    for i, (k, v) in enumerate(d.items()):
-        if i > 0:
-            agg.append(", ")
-        agg.append(k)
-        agg.append(": ")
-        _any_preview(v, agg)
+    def _append_dict(self, d: dict[str, Any], brackets: bool):
+        # For simplification, we consider that any dict
+        # with a "content_type" key is a file and should have a specific preview
+        if "content_type" in d and ("url" in d or "data" in d or "storage_url" in d):
+            self._append_file(d["content_type"], d.get("url"), d.get("storage_url"))
+            return
+        if brackets:
+            self.append("{")
 
+        for i, (k, v) in enumerate(d.items()):
+            if i > 0:
+                self.append(", ")
+            self.append(k)
+            self.append(": ")
+            self._append_any(v)
 
-def _list_preview(arr: list[Any], agg: _Agg):
-    for i, v in enumerate(arr):
-        if i > 0:
-            agg.append(", ")
-        _any_preview(v, agg)
+        if brackets:
+            self.append("}")
+
+    def _append_list(self, arr: list[Any]):
+        for i, v in enumerate(arr):
+            if i > 0:
+                self.append(", ")
+            self._append_any(v)
+
+    def build(self, value: Any):
+        try:
+            # Not use _any_preview to avoid adding quotes to strings, etc.
+            if isinstance(value, dict):
+                self._append_dict(value, brackets=False)  # type: ignore
+            elif isinstance(value, list):
+                self._append_list(value)  # type: ignore
+            else:
+                self.append(value)
+        except _MaxLenReached:
+            pass
+
+        return str(self)
 
 
 def compute_preview(model: Any, max_len: int = 255) -> str:
+    """Compute a preview for a given object. All exceptions are handled and a fallback is returned."""
     if not model:
         return "-"
 
-    if not isinstance(model, BaseModel):
-        model = RootModel(model)
+    if isinstance(model, BaseModel):
+        model = model.model_dump(exclude_none=True, mode="json")
 
-    dumped = model.model_dump(exclude_none=True, mode="json")
-
-    agg = _Agg(max_len)
     try:
-        # Not use _any_preview to avoid adding quotes to strings, etc.
-        if isinstance(dumped, dict):
-            _dict_preview(dumped, agg)  # type: ignore
-        elif isinstance(dumped, list):
-            _list_preview(dumped, agg)  # type: ignore
-        else:
-            agg.append(dumped)
-    except _MaxLenReached:
-        pass
-    return str(agg)
+        return _Agg(max_len).build(model)
+    except Exception:
+        logging.getLogger(__name__).exception("error computing preview", extra={"model": model})
+        return "..."
